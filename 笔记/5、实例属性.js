30821a64651bc08dfb076c8fc93cf7725e40228e
weitianci <947846776@qq.com>
2019-08-31 17:58:54 +0800

    。

--- 笔记/5、实例属性.js
+++ 笔记/5、实例属性.js
@@ -1,12 +1,12 @@
 // $el: 挂载的DOM元素
-// $options: 当前的实例的参数；
-// $refs: 获取DOM元素；
-// $set : 向data的对象新增一些属性；
-// $delete
+// $options: 当前的实例的参数
+// $refs: 获取DOM元素
+// $set : 向data的对象新增一些属性
+// $delete ：删除
 // $destroy ： 销毁vue的实例
 // $mount : 挂载
 // $on : 订阅
 // $emit : 发布
-// $off : 移出；
-// $forceUpdate: 强制更新；不能在beforeUpdate和updated使用；会递归；
-// $nextTick:
+// $off : 移出
+// $forceUpdate: 强制更新，不能在beforeUpdate和updated使用，会递归
+// $nextTick
