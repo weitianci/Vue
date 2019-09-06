3d90a11e6a499abb512d2d5be776e885c875a119
weitianci <947846776@qq.com>
2019-09-06 19:19:08 +0800

    .

--- simple/app/greet.js
+++
@@ -1,6 +0,0 @@
-// common.js规范，node就是common.js规范
-module.exports = function () {
-    let greet = document.createElement("div");
-    greet.innerHTML = "hello world";
-    return greet;
-};
--- simple/app/main.js
+++
@@ -1,8 +0,0 @@
-// require导入，把greet.js中导出的内容接收到
-const gre = require("./greet.js")
-let fn = ()=>{
-
-};
-fn();
-import"../css/main.css";
-document.getElementById("root").appendChild(gre());
